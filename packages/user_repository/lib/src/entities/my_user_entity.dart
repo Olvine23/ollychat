85f513f57c31835521b7e9999dc727d92eda750c
Olvine23 <olivinegeorge@gmail.com>
2024-03-20 10:48:11 +0300

    Created a user model class

--- packages/user_repository/lib/src/entities/my_user_entity.dart
+++ packages/user_repository/lib/src/entities/my_user_entity.dart
@@ -1,3 +1,9 @@
-class MyUserEntity{
-  
+class MyUserEntity {
+  final String name;
+  final String? image;
+  final String email;
+  final String id;
+
+  MyUserEntity(
+      {required this.name, this.image, required this.email, required this.id});
 }
--- packages/user_repository/lib/src/models/my_user.dart
+++ packages/user_repository/lib/src/models/my_user.dart
@@ -1,8 +1,58 @@
 import 'package:equatable/equatable.dart';
+import 'package:user_repository/src/entities/entities.dart';
 
-class MyUser extends Equatable{
+class MyUser extends Equatable {
+  final String id;
+  final String email;
+  final String name;
+  final String? image;
+
+  const MyUser(
+      {required this.id, required this.email, required this.name, this.image});
+
+  // unauthenticated user i.e with empty values
+
+  static const empty = MyUser(id: '', email: '', name: '', image: '');
+
+  //for modification of MyUser params
+  MyUser copyWith({
+    String? id,
+    String? email,
+    String? name,
+    String? image,
+  }) {
+    return MyUser(
+        id: id ?? this.id,
+        email: email ?? this.email,
+        name: name ?? this.name,
+        image: image ?? this.image);
+  }
+
+  //getter that checks whether a user is empty
+  bool get isEmpty => this == MyUser.empty;
+
+//getter that checks whether a user is not empty
+
+  bool get isNotEmpty => this != MyUser.empty;
+
+  MyUserEntity toEntity(){
+    return MyUserEntity(
+      id:id,
+      email:email,
+      name:name,
+      image:image
+    );
+  }
+
+  static MyUser fromEntity(MyUserEntity entity){
+    return MyUser(
+      id: entity.id,
+      name: entity.name,
+      image: entity.image,
+      email: entity.email);
+  }
+
   @override
   // TODO: implement props
-  List<Object?> get props => [];
-
+  List<Object?> get props => [id, email, name, image];
 }
