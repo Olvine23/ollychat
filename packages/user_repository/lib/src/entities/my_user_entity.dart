324c45512e8315713ce70961fc87d6588de29827
Olvine23 <olivinegeorge@gmail.com>
2024-03-20 11:04:10 +0300

    Created entities for the user repository

--- packages/user_repository/lib/src/entities/my_user_entity.dart
+++ packages/user_repository/lib/src/entities/my_user_entity.dart
@@ -1,4 +1,6 @@
-class MyUserEntity {
+import 'package:equatable/equatable.dart';
+
+class MyUserEntity extends Equatable {
   final String name;
   final String? image;
   final String email;
@@ -6,4 +8,43 @@ class MyUserEntity {
 
   MyUserEntity(
       {required this.name, this.image, required this.email, required this.id});
+
+  //create a toJson object
+  Map<String, Object> toJson() {
+    return {
+      'id': id,
+      'image': image!,
+      'email': email,
+      'name': name,
+    };
+  }
+
+  //create a fromJson object
+
+  static MyUserEntity fromJson(Map<String, dynamic> doc) {
+    return MyUserEntity(
+        name: doc['name'] as String,
+        email: doc['email'] as String,
+        id: doc['id'] as String,
+        image: doc['image'] as String?);
+  }
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [name, id, image, email];
+
+  @override
+  String toString() {
+    return '''UserEntity:{
+
+      id:$id,
+      email:$email,
+      name:$name,
+      image:$image
+
+    }
+
+
+''';
+  }
 }
