4a5394bc33b3ebe9ac6aec72de5738330170058a
Olvine23 <olivinegeorge@gmail.com>
2024-03-21 13:16:44 +0300

    Created custom textfield widget

--- lib/components/custom_textfield.dart
+++ lib/components/custom_textfield.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:olly_chat/theme/colors.dart';
 
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
@@ -29,32 +30,48 @@ class CustomTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   TextFormField(
-      validator: validator,
-      controller: controller,
-      obscureText: obscureText,
-			keyboardType: keyboardType,
-			focusNode: focusNode,
-			onTap: onTap,
-			textInputAction: TextInputAction.next,
-			onChanged: onChanged,
-      decoration: InputDecoration(
-				suffixIcon: suffixIcon,
-				prefixIcon: prefixIcon,
-				enabledBorder: OutlineInputBorder(
-					borderRadius: BorderRadius.circular(10),
-					borderSide: const BorderSide(color: Colors.transparent),
-				),
-				focusedBorder: OutlineInputBorder(
-					borderRadius: BorderRadius.circular(20),
-					borderSide: BorderSide(color: Theme.of(context).colorScheme.secondary),
-				),
-				fillColor: Colors.grey.shade200,
-				filled: true,
-				hintText: hintText,
-				hintStyle: TextStyle(color: Colors.grey[500]),
-				errorText: errorMsg,
-			)
+    return Container(
+        decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: AppColors.greenWhite, // Shadow color
+                          spreadRadius: 5, // Spread radius
+                          blurRadius: 30, // Blur radius
+                          offset: Offset(0, 5), // Offset
+                        ),
+                      ],
+                    ),
+
+      child: TextFormField(
+          validator: validator,
+          controller: controller,
+          obscureText: obscureText,
+          keyboardType: keyboardType,
+          focusNode: focusNode,
+          onTap: onTap,
+          textInputAction: TextInputAction.next,
+          onChanged: onChanged,
+          decoration: InputDecoration(
+            suffixIcon: suffixIcon,
+            prefixIcon: prefixIcon,
+             
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide:
+                  BorderSide(
+                    width: 1.0,
+                    color:  AppColors.primaryColor),
+            ),
+            errorBorder: const UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.red)),
+            // fillColor: Colors.grey.shade200,
+            filled: true,
+            hintText: hintText,
+            hintStyle: TextStyle(color: Colors.grey[500]),
+            errorText: errorMsg,
+          )),
     );
   }
 }
---
+++ lib/theme/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static Color primaryColor = const Color(0Xff5E2CA5);
+  static Color  primaryRedColor =  const Color(0xffFF0303);
+  static Color secondaryColor =  const Color(0xffB1816D);
+  static Color darkBlue = const Color(0x0ff2222c);
+  static Color frenchGray = const Color(0xffb9bbc9);
+  static Color white = const Color(0xffffffff);
+  static Color black = const Color(0xff000000);
+  static Color darkGrey = const Color(0xff535865);
+  static Color greenWhite = const Color(0xffe9e9e9);
+  static Color charcoalGrey = const Color(0xff414141);
+
+}
