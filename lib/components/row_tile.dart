2f866523fde28f71eaafe7d1988f46eb8473e3a2
Olvine23 <olivinegeorge@gmail.com>
2024-04-18 22:18:11 +0300

    fix pixel overflow @profile-screen()

--- lib/components/row_tile.dart
+++ lib/components/row_tile.dart
@@ -20,8 +20,8 @@ class RowTile extends StatelessWidget {
         child: Row(
           children: [
             Container(
-                height: 150,
-                width: 180,
+                height: 120,
+                width: 150,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
                     image: DecorationImage(
@@ -35,54 +35,56 @@ class RowTile extends StatelessWidget {
                 )),
     
                 SizedBox(width: 16.dp,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(title, style: Theme.of(context)
-                  .textTheme
-                  .bodyLarge!
-                  .copyWith(fontWeight: FontWeight.bold, fontSize: 18),),
-                    SizedBox(height: 8,),
-                    Row(
-                      children: [
-                        CircleAvatar(
-                          backgroundImage: NetworkImage(userAvatar),
-              radius: 20,
-              backgroundColor: AppColors.primaryColor,
-             
-            ),
-            SizedBox(width: 4.dp,),
-            Text(
-                  authorName,
-                  
-                  textAlign: TextAlign.justify,
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodySmall!
-                      .copyWith(
-                        fontWeight: FontWeight.bold),
-                ),
-                      ],
-                    ),
-                    SizedBox(height: 8,),
-                    Row(
-    
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("2 days ago"),
-                        SizedBox(width: 12.dp,),
-                        Container(
-                          child: 
-                          Row(
-                           
-                            children: [
-                            Icon(Icons.bookmark),
-                            IconButton(onPressed:() {}, icon:Icon(Icons.more_horiz))
-                          ]),
-                        )
-                      ],
-                    )
-                  ],
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(title, style: Theme.of(context)
+                    .textTheme
+                    .bodyLarge!
+                    .copyWith(fontWeight: FontWeight.bold, fontSize: 18),),
+                      SizedBox(height: 8,),
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(userAvatar),
+                                radius: 20,
+                                backgroundColor: AppColors.primaryColor,
+                               
+                              ),
+                              SizedBox(width: 4.dp,),
+                              Text(
+                    authorName,
+                    
+                    textAlign: TextAlign.justify,
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodySmall!
+                        .copyWith(
+                          fontWeight: FontWeight.bold),
+                  ),
+                        ],
+                      ),
+                      SizedBox(height: 8,),
+                      Row(
+                      
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("2 days ago"),
+                          SizedBox(width: 12.dp,),
+                          Container(
+                            child: 
+                            Row(
+                             
+                              children: [
+                              Icon(Icons.bookmark),
+                              IconButton(onPressed:() {}, icon:Icon(Icons.more_horiz))
+                            ]),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
           ],
         ),
--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -3,10 +3,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_sizer/flutter_sizer.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:olly_chat/blocs/get_post/get_post_bloc.dart';
 import 'package:olly_chat/blocs/myuserbloc/myuser_bloc.dart';
 import 'package:olly_chat/blocs/sign_in/sign_in_bloc.dart';
 import 'package:olly_chat/blocs/updateuserinfo/update_user_info_bloc.dart';
+import 'package:olly_chat/components/row_tile.dart';
+import 'package:olly_chat/screens/home/widgets/shimmer_widget.dart';
 import 'package:olly_chat/theme/colors.dart';
+import 'package:shimmer/shimmer.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -27,138 +31,249 @@ class _ProfileScreenState extends State<ProfileScreen> {
           });
         }
       },
-      child: Scaffold(
-        appBar: AppBar(
-          leading: Image.asset(
-            'assets/images/nobg.png',
-            height: 100,
+      child: DefaultTabController(
+        
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            leading: Image.asset(
+              'assets/images/nobg.png',
+              height: 100,
+            ),
+            centerTitle: false,
+            elevation: 0,
+            backgroundColor: Theme.of(context).colorScheme.background,
+            title: Text(
+              "Profile ",
+              style: Theme.of(context)
+                  .textTheme
+                  .titleLarge!
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    context.read<SignInBloc>().add(SignOutRequired());
+                  },
+                  icon: Icon(
+                    Icons.logout,
+                    color: Theme.of(context).colorScheme.onBackground,
+                  )),
+              IconButton(
+                  onPressed: () {
+                    context.read<SignInBloc>().add(SignOutRequired());
+                  },
+                  icon: Icon(
+                    Icons.settings,
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ))
+            ],
           ),
-          centerTitle: false,
-          elevation: 0,
-          backgroundColor: Theme.of(context).colorScheme.background,
-          title: Text(
-            "Profile ",
-            style: Theme.of(context)
-                .textTheme
-                .titleLarge!
-                .copyWith(fontWeight: FontWeight.bold),
-          ),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  context.read<SignInBloc>().add(SignOutRequired());
-                },
-                icon: Icon(
-                  Icons.logout,
-                  color: Theme.of(context).colorScheme.onBackground,
-                )),
-            IconButton(
-                onPressed: () {
-                  context.read<SignInBloc>().add(SignOutRequired());
-                },
-                icon: Icon(
-                  Icons.settings,
-                  color: Theme.of(context).colorScheme.onBackground,
-                ))
-          ],
-        ),
-        body: BlocBuilder<MyUserBloc, MyUserState>(
-          builder: (context, state) {
-            if (state.status == MyUserStatus.success) {
-              return Column(
-                children: [
-                  ListTile(
-                    leading: state.user!.image == ""
-                        ? GestureDetector(
-                            onTap: () async {
-                              final ImagePicker picker = ImagePicker();
-                              final XFile? image = await picker.pickImage(
-                                  source: ImageSource.gallery,
-                                  maxHeight: 500,
-                                  maxWidth: 500,
-                                  imageQuality: 40);
-                              if (image != null) {
-                                CroppedFile? croppedFile =
-                                    await ImageCropper().cropImage(
-                                  sourcePath: image.path,
-                                  aspectRatio: const CropAspectRatio(
-                                      ratioX: 1, ratioY: 1),
-                                  aspectRatioPresets: [
-                                    CropAspectRatioPreset.square
-                                  ],
-                                  uiSettings: [
-                                    AndroidUiSettings(
-                                        toolbarTitle: 'Cropper',
-                                        toolbarColor: Theme.of(context)
-                                            .colorScheme
-                                            .primary,
-                                        toolbarWidgetColor: Colors.white,
-                                        initAspectRatio:
-                                            CropAspectRatioPreset.original,
-                                        lockAspectRatio: false),
-                                    IOSUiSettings(
-                                      title: 'Cropper',
+          body: SingleChildScrollView(
+            
+            child: Column(
+              children: [
+                SizedBox(height: 8.dp,),
+        
+                BlocBuilder<MyUserBloc, MyUserState>(
+                  builder: (context, state) {
+                    if (state.status == MyUserStatus.success) {
+                      return Column(
+                        children: [
+                          ListTile(
+                            leading: state.user!.image == ""
+                                ? GestureDetector(
+                                    onTap: () async {
+                                      final ImagePicker picker = ImagePicker();
+                                      final XFile? image = await picker.pickImage(
+                                          source: ImageSource.gallery,
+                                          maxHeight: 500,
+                                          maxWidth: 500,
+                                          imageQuality: 40);
+                                      if (image != null) {
+                                        CroppedFile? croppedFile =
+                                            await ImageCropper().cropImage(
+                                          sourcePath: image.path,
+                                          aspectRatio: const CropAspectRatio(
+                                              ratioX: 1, ratioY: 1),
+                                          aspectRatioPresets: [
+                                            CropAspectRatioPreset.square
+                                          ],
+                                          uiSettings: [
+                                            AndroidUiSettings(
+                                                toolbarTitle: 'Cropper',
+                                                toolbarColor: Theme.of(context)
+                                                    .colorScheme
+                                                    .primary,
+                                                toolbarWidgetColor: Colors.white,
+                                                initAspectRatio:
+                                                    CropAspectRatioPreset.original,
+                                                lockAspectRatio: false),
+                                            IOSUiSettings(
+                                              title: 'Cropper',
+                                            ),
+                                          ],
+                                        );
+                                        if (croppedFile != null) {
+                                          setState(() {
+                                            context.read<UpdateUserInfoBloc>().add(
+                                                UploadPicture(
+                                                    croppedFile.path,
+                                                    context
+                                                        .read<MyUserBloc>()
+                                                        .state
+                                                        .user!
+                                                        .id));
+                                          });
+                                        }
+                                      }
+                                    },
+                                    child: Container(
+                                      width: 50,
+                                      height: 50,
+                                      decoration: BoxDecoration(
+                                          color: Colors.grey.shade300,
+                                          shape: BoxShape.circle),
+                                      child: Icon(Icons.person,
+                                          color: Colors.grey.shade400),
                                     ),
-                                  ],
-                                );
-                                if (croppedFile != null) {
-                                  setState(() {
-                                    context.read<UpdateUserInfoBloc>().add(
-                                        UploadPicture(
-                                            croppedFile.path,
-                                            context
-                                                .read<MyUserBloc>()
-                                                .state
-                                                .user!
-                                                .id));
-                                  });
-                                }
-                              }
-                            },
-                            child: Container(
-                              width: 50,
-                              height: 50,
-                              decoration: BoxDecoration(
-                                  color: Colors.grey.shade300,
-                                  shape: BoxShape.circle),
-                              child: Icon(Icons.person,
-                                  color: Colors.grey.shade400),
+                                  )
+                                : Container(
+                                    width: 50.dp,
+                                    height: 50.dp,
+                                    decoration: BoxDecoration(
+                                        color: Colors.red,
+                                        shape: BoxShape.circle,
+                                        image: DecorationImage(
+                                            image: NetworkImage(
+                                              state.user!.image!,
+                                            ),
+                                            fit: BoxFit.cover)),
+                                  ),
+                            title: Text(
+                              state.user!.name,
+                              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                  fontSize: 18.dp, fontWeight: FontWeight.bold),
                             ),
-                          )
-                        : Container(
-                            width: 50.dp,
-                            height: 50.dp,
-                            decoration: BoxDecoration(
-                                color: Colors.red,
-                                shape: BoxShape.circle,
-                                image: DecorationImage(
-                                    image: NetworkImage(
-                                      state.user!.image!,
-                                    ),
-                                    fit: BoxFit.cover)),
+                            subtitle: Text("@olly_doe"),
+                            trailing: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    foregroundColor: Colors.white,
+                                    backgroundColor: AppColors.primaryColor),
+                                onPressed: () {},
+                                child: Text("Edit")),
                           ),
-                    title: Text(
-                      state.user!.name,
-                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                          fontSize: 18.dp, fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text("@olly_doe"),
-                    trailing: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            foregroundColor: Colors.white,
-                            backgroundColor: AppColors.primaryColor),
-                        onPressed: () {},
-                        child: Text("Edit")),
+                        ],
+                      );
+                    } else if (state.status == MyUserStatus.failure) {
+                      return Center(
+                        child: Text(state.status.toString()),
+                      );
+                    }
+                    return const Center(child: CircularProgressIndicator());
+                  },
+                ),
+                SizedBox(height: 8.dp,),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 0.dp),
+                  child: Column(
+                    children: [
+                      const Divider(
+                        thickness: 0.2,
+                        
+                      ),
+                      SizedBox(height: 8.dp,),
+        
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+        
+                          Column(
+                            children: [
+                              Text("125",style: Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.bold),),
+                              SizedBox(height: 4.dp,),
+                              Text("Articles")
+        
+                            ],
+                          ),
+                          Column(children: [
+                            Text("105",style: Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.bold),),
+                            SizedBox(height: 4.dp,),
+                            Text("Following")
+        
+                          ],),
+                          Column(children: [
+                            Text("23334",style: Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.bold),),
+                            SizedBox(height: 4.dp,),
+                            Text("Followers")
+        
+                          ],),
+        
+        
+                        ],
+                      ),
+                         const Divider(
+                        thickness: 0.2,
+                        
+                      ),
+
+                      TabBar(tabs: [
+
+                        Tab(text: "Articles",),
+                        Tab(text: "About",)
+
+                      ]),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height + 600,
+                        child: TabBarView(children: [
+                           BlocBuilder<GetPostBloc, GetPostState>(
+              builder: (context, state) {
+                if (state.status == GetPostStatus.success) {
+                  return ListView.builder(
+                    shrinkWrap: true, // Add this line
+                    physics:
+                        const NeverScrollableScrollPhysics(), // Add this line
+                    itemCount: state.posts?.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () {
+                          // Handle onTap
+                        },
+                        child: RowTile(
+                          imageUrl: state.posts![index].thumbnail!,
+                          title: state.posts![index].title,
+                          userAvatar: state.posts![index].myUser.image!,
+                          authorName: state.posts![index].myUser.name,
+                        ),
+                      );
+                    },
+                  );
+                } else if (state.status == GetPostStatus.unknown) {
+                  return Shimmer.fromColors(
+                    highlightColor: Colors.white54,
+                    baseColor: const Color(0xffdedad7),
+                    child: project_screen_shimmer(context),
+                  );
+                }
+                return const Text("No data available"); // Handle other states
+              },
+            ),
+                          Text("About")
+                          
+                        ]),
+                      )
+
+
+        
+                    ],
                   ),
-                ],
-              );
-            } else if (state.status == MyUserStatus.failure) {
-              return Center(
-                child: Text(state.status.toString()),
-              );
-            }
-            return const Center(child: CircularProgressIndicator());
-          },
+                  
+                )
+              ],
+            ),
+          ),
+        
+          
         ),
       ),
     );
