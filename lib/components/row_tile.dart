d3471651e1be346d6986df64c9c3d9694dd59f3c
Olvine23 <olivinegeorge@gmail.com>
2024-04-18 23:40:24 +0300

    Added Cached Network Image in profile section

--- lib/components/row_tile.dart
+++ lib/components/row_tile.dart
@@ -1,5 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sizer/flutter_sizer.dart';
+import 'package:lottie/lottie.dart';
 import 'package:olly_chat/theme/colors.dart';
 
 class RowTile extends StatelessWidget {
@@ -19,20 +21,37 @@ class RowTile extends StatelessWidget {
       child: Container(
         child: Row(
           children: [
-            Container(
-                height: 120,
-                width: 150,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    image: DecorationImage(
-                        image: NetworkImage(
-                             imageUrl),
-                        fit: BoxFit.cover)),
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      ),
-                )),
+             Container(
+                  height: 18.h,
+                  width: 40.w,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(20)),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(
+                        10.0), // Adjust the value as needed
+                    child: CachedNetworkImage(
+                      imageUrl: imageUrl ,
+                      placeholder: (context, url) =>
+                          Center(child: Lottie.asset('assets/lotti/imageload.json')),
+                      errorWidget: (context, url, error) => Icon(Icons.error),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+            // Container(
+            //     height: 120,
+            //     width: 150,
+            //     decoration: BoxDecoration(
+            //         borderRadius: BorderRadius.circular(20),
+            //         image: DecorationImage(
+            //             image: NetworkImage(
+            //                  imageUrl),
+            //             fit: BoxFit.cover)),
+            //     child: Container(
+            //       decoration: BoxDecoration(
+            //           borderRadius: BorderRadius.circular(20),
+            //           ),
+            //     )),
     
                 SizedBox(width: 16.dp,),
                 Expanded(
